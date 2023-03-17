5409cafe80959df2b3a13b02bb133a11afd5511f
KAZUKI KOBAYASHI <kazscript@gmail.com>
2023-03-17 14:23:33 +0900

    first

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import streamlit as st
+import pandas as pd
+import os
 
-st.checkbox("チェックボックス") #引数に入れることでboolを返す
-st.button("ボタン") #引数に入れるとboolで返す
-st.selectbox("メニューリスト", ("選択肢1", "選択肢2", "選択肢3")) #第一引数：リスト名、第二引数：選択肢
-st.multiselect("メニューリスト（複数選択可）", ("選択肢1", "選択肢2", "選択肢3")) #第一引数：リスト名、第二引数：選択肢、複数選択可
-st.radio("ラジオボタン", ("選択肢1", "選択肢2", "選択肢3")) #第一引数：リスト名（選択肢群の上に表示）、第二引数：選択肢
-# 以下をサイドバーに表示
-st.sidebar.text_input("文字入力欄") #引数に入力内容を渡せる
-st.sidebar.text_area("テキストエリア")
+# ページタイトルの設定
+st.set_page_config(page_title="ToDo App", page_icon=":memo:", layout="wide")
+
+# ファイルが存在しない場合は作成する
+if not os.path.exists("tasks.csv"):
+    with open("tasks.csv", "w") as f:
+        f.write("task\n")
+
+# CSVファイルを読み込む
+df = pd.read_csv("tasks.csv")
+
+# セッションステートの初期化
+if "tasks" not in st.session_state:
+    st.session_state.tasks = df["task"].tolist()
+
+# サイドバーにタスクの入力フォームを表示
+task = st.sidebar.text_input("Add Task")
+
+# タスクを追加するためのボタン
+if st.sidebar.button("Add"):
+    st.session_state.tasks.append(task)
+
+    # CSVファイルに書き込む
+    pd.DataFrame({"task": st.session_state.tasks}).to_csv("tasks.csv", index=False)
+
+# タスクリストの表示
+st.header("Task List")
+if not st.session_state.tasks:
+    st.write("No tasks yet.")
+else:
+    for i, task in enumerate(st.session_state.tasks):
+        st.write(f"{i+1}. {task}")
+
+# クリアボタン
+if st.button("Clear All"):
+    st.session_state.tasks = []
+
+    # CSVファイルを空にする
+    pd.DataFrame({"task": st.session_state.tasks}).to_csv("tasks.csv", index=False)
